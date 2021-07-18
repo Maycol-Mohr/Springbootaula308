f98f0edd5e879b32d122f30ed011ddce42cd655c
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-07-18 20:59:50 +0200

    Project created

---
+++ src/main/java/com/educandoweb/courseaula308/Courseaula308Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseaula308;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Courseaula308Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Courseaula308Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/courseaula308/Courseaula308ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseaula308;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Courseaula308ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
