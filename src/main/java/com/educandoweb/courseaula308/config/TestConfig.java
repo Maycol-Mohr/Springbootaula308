3f3affa96fbd0541ba73170c2c8844133913e321
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-07-20 11:43:03 +0200

    Payment, one-to-one association

--- src/main/java/com/educandoweb/courseaula308/config/TestConfig.java
+++ src/main/java/com/educandoweb/courseaula308/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.courseaula308.entities.Category;
 import com.educandoweb.courseaula308.entities.Order;
 import com.educandoweb.courseaula308.entities.OrderItem;
+import com.educandoweb.courseaula308.entities.Payment;
 import com.educandoweb.courseaula308.entities.Product;
 import com.educandoweb.courseaula308.entities.User;
 import com.educandoweb.courseaula308.entities.enums.OrderStatus;
@@ -86,6 +87,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 
